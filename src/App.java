e19587fff9b7c564eee04eec6e7d19ff08b5dc73
Ray Klump Lewis University <klumpra@lewisu.edu>
2021-11-22 11:36:49 -0600

    Added a comment to the main file.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
 	public static void main(String[] args) {
+		// opens the draw choice frame and shows a circle initially
 		DrawChoiceFrame dcf = new DrawChoiceFrame();
 		dcf.setVisible(true);
 	}
